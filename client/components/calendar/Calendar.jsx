ac9851d16397d0a2d429a945f0520841e5597f89
Kianna <kiannaquach.kq@gmail.com>
2018-08-12 21:55:46 -0700

    Add prev and next buttons to change month and year

--- client/components/calendar/Calendar.jsx
+++ client/components/calendar/Calendar.jsx
@@ -1,18 +1,62 @@
 import React from 'react';
 import moment from 'moment';
+import CalendarDayNames from './CalendarDayNames';
 
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      momentDate: moment(),
+      selectedDay: moment().startOf('day')
+    };
+
+    this.getPrevious = this.getPrevious.bind(this);
+    this.getNext = this.getNext.bind(this);
+  }
+
+  /******  MONTH & YEAR & PREV/NEXT HEADER ******/
+  // get month name/year using moment format method
+  getMonthName() {
+    return (
+      <span className="month-name">
+        {this.state.momentDate.format("MMMM")}
+        <span> </span>
+        <span className="month-year">
+          {this.state.momentDate.format("YYYY")}
+        </span>
+      </span>
+    );
+  }
+
+  // onClick you get prev month
+  // setState to month - 1
+  // use moment method subtract to increment month
+  getPrevious() {
+    this.setState({
+      momentDate: this.state.momentDate.subtract(1, 'month')
+    });
   }
 
 
+  // onClick you get next month
+  // setState to month - 1
+  // use moment method subtract to increment month
+  getNext() {
+    this.setState({
+      momentDate: this.state.momentDate.add(1, 'month')
+    });
+  }
+
   render() {
-    console.log(this.state.month);
     return (
-      <div>
-        rendered
+      <div className="calendar">
+          <div className="month row">
+            {this.getMonthName()}
+            <i className="arrow fa fa-angle-left" onClick={this.getPrevious}/>
+            <i className="arrow fa fa-angle-right" onClick={this.getNext}/>
+          </div>
+          <CalendarDayNames />
       </div>
     );
   }
