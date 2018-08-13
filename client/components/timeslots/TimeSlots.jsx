0ebf3d54b7b50d7ed3e45a21b1acdf8d7829e62b
Kianna <kiannaquach.kq@gmail.com>
2018-08-13 13:48:36 -0700

    Render corresponding time slot information to third column

--- client/components/timeslots/TimeSlots.jsx
+++ client/components/timeslots/TimeSlots.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import data from '../../data/exampleTimeSlotData.json';
+import TimeSlot from './TimeSlot';
 
 class TimeSlots extends React.Component {
   constructor(props) {
@@ -23,19 +24,26 @@ class TimeSlots extends React.Component {
 
   render() {
     return (
-      <div className="middle-grid">
-      {
-        data.timeslots.map((timeSlot) => {
-          return (
-            <div className="timeslot" onClick={() => this.bookActivityInfo()} key={timeSlot.id}>
-              <div>{timeSlot.activity_name}</div>
-              <div>start: {timeSlot.start.slice(11)}</div>
-              <div>end: {timeSlot.end.slice(11)}</div>
-              <br />
-            </div>
-          );
-        })
-      }
+      <div className="grid">
+        <div className="middle-grid grid__wrapper">
+        {
+          data.timeslots.map((timeSlot) => {
+            return (
+              <div>
+                <div className="timeslot" key={timeSlot.id} onClick={() => this.bookSelectedActivity(timeSlot)}>
+                  <div>{timeSlot.activity_name}</div>
+                  <div>start: {timeSlot.start.slice(11)}</div>
+                  <div>end: {timeSlot.end.slice(11)}</div>
+                  <br />
+                </div>
+              </div>
+            );
+          })
+        }
+        </div>
+
+        <TimeSlot selectedActivity={this.state.selectedActivity} />
+
       </div>
     );
   }
